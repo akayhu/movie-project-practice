6b43f02943c0da5d65b0dd09696b266d2891b949
akay.hu[胡凱綺] <akay.hu@104.com.tw>
2019-02-14 15:14:15 +0800

    新增ProcessReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux-immutable';
 import MovieReducer from './movie';
 import TestReducer from './test';
+import ProcessReducer from './process';
 
 const rootReducer = combineReducers({
   movie: MovieReducer,
-  test: TestReducer
+  test: TestReducer,
+  Process: ProcessReducer
 });
 
 export default rootReducer;
